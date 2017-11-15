0b8b31b27cc32e6f00cc13722a2b4bd7040ecb16
mabdurachmans <32905593+mabdurachmans@users.noreply.github.com>
2017-11-15 12:17:13 +0100

    Update Ising_Model.R

--- Ising_Model.R
+++ Ising_Model.R
@@ -6,66 +6,45 @@
 #in the matrix every row repreasents a system.
 
 get_random<-function(x){
-l<-0                                             # get initial l 0
-r<-0                                             # get initial r 0
-h<-sample(c(-1,1),ncol(x),replace=T)             # get a random system-t
-t<-matrix(h,nrow=1,ncol= ncol(x))                
-for(j in 1:nrow(x))                              # compared t to every existed system
-{
-for(k in 1:ncol(x)){if (x[j,k]!=t[1,k]){l=l+1}}      ## campared every element between t and existed system [j,]. l represents the number of different elements.
-if (l>0){r=r+1}                                      ##if l>1, which means system j is different with t ,r+1  r is  the number of different system.
-l<-0                                                 ##l come to zero and get next comparing
-}
-if(r==nrow(x)){x<-rbind(x,t)}                    # if the number of different systems equal to the number of existed systems, add t into the matrix
+  l<-0                                             # get initial l 0
+  r<-0                                             # get initial r 0
+  h<-sample(c(-1,1),ncol(x),replace=T)             # get a random system-t
+  t<-matrix(h,nrow=1,ncol= ncol(x))                
+  for(j in 1:nrow(x)){                              # compared t to every existed system
+      for(k in 1:ncol(x)){if (x[j,k]!=t[1,k]){l=l+1}}      ## campared every element between t and existed system [j,]. l represents the number of different elements.
+    if (l>0){r=r+1}                                      ##if l>1, which means system j is different with t ,r+1  r is  the number of different system.
+    l<-0                                                 ##l come to zero and get next comparing
+    }
+  if(r==nrow(x)){x<-rbind(x,t)}                    # if the number of different systems equal to the number of existed systems, add t into the matrix
 return(x)}                                       # retrun the operated matrix.
 
-
-
-
-
-
-
-
-
 #function: Generate N random a*b system and form a matrix. every row of the matrix repreasents a system.
-
-
 get_ns<-function(a,b,n){
-d<-sample(c(-1,1),a*b,replace=T)                     #get a random a*b system x
-x<-matrix(d,1,a*b)
-z<-0                                                 # generate a system q which is different with x
+  d<-sample(c(-1,1),a*b,replace=T)                     #get a random a*b system x
+  x<-matrix(d,1,a*b)
+  z<-0                                                 # generate a system q which is different with x
                                                      ## set z as a initial value 0 to active the loop random generator, z is the number of a different system elements
-while(z==0){                                         ## if the q is same with x 
-z<-0                                                 ## set z=0 
-q<-matrix(sample(c(-1,1),a*b,replace=T),1,a*b)       ## get a random system q 
-for(m in 1:a*b){if (x[1,m]!=q[1,m]){z=z+1            ## compare q to x, z is the number of the diiffrent elements
-}}
-}
+  while(z==0){                                         ## if the q is same with x 
+    z<-0                                                 ## set z=0 
+    q<-matrix(sample(c(-1,1),a*b,replace=T),1,a*b)       ## get a random system q 
+    for(m in 1:a*b){if (x[1,m]!=q[1,m]){z=z+1}            ## compare q to x, z is the number of the diiffrent elements
+  }
+ }
+  
 x<-rbind(x,q)                                        # add q into x.
 i<-2                                                 # i: the number of system is 2
 while(i<n){
-if(nrow(x)==i){x<-get_random(x)                      # for row i of the matrix, get a random system 
-i=i+1}                                               # if the random system is same with existed system, redo row i or do i+1
-}
+  if(nrow(x)==i){x<-get_random(x)                      # for row i of the matrix, get a random system 
+  i=i+1}                                               # if the random system is same with existed system, redo row i or do i+1
+  }
 return(x)}                                           # return x
-
-
-
-
-
-
 
 #selects a system of x and make it into a a*b matrix.
 make_m<-function(a,b,x){
 h<-matrix(x,a,b)
 return(h)}
 
-
-
-
-
 #get |m|(s)=1/(LR*LC)|SUM Sx| of a system n.
-
 m_s<-function(n){
 h<-0
 x<-0
@@ -76,11 +55,6 @@ h=h+n[i,j]}
 x=1/(nrow(n)*ncol(n))*abs(h)    #get |m|(s)=1/(LR*LC)|SUM Sx|  .  
 return(x)
 }
-
-
-
-
-
 #get m bar for random system.
 #n is the number of the random system.a,b is the dimansion of the matrix.
 
@@ -99,8 +73,6 @@ M=s/s2                           # m bar = numerator/denominator
 return(M)                        # return M
 }  
 
-
-
 #m bar of a  stable system with n RANDOM SYSTEM.
 x<-get_ns(a,b,n)               #get a random system group
 m_bar_n<-function(a,b,x){
@@ -116,8 +88,6 @@ s2<-s2+exp((-0.5)*get_H(h))    #calculate the sum of diamenator
 M=s/s2                         # m bar = numerator/denominator
 return(M)                      #return M
 }
-
-
 
 #Metropolis algotithm
 M_a<-function(x){
@@ -138,11 +108,6 @@ else{s_1<-x_i}
 return(s_1)                               #turn to the transform system
 }
 
-
-
-
-
-
 # simple m bar calculation
 m_average_simple<-function(a,b,N){
 x<-get_ns(a,b,N)             # get N system
@@ -153,8 +118,6 @@ S<-S+m_s(h)                  # Sum the |m|(s) of all the system
 }
 M<-S/N                       # m bar = Sum the |m|(s)/N
 return(M)}
-
-
 
 d<-sample(c(-1,1),100*100,replace=T)
 x<-matrix(d,100,100)
